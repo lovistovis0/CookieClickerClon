c836b4af116537c9406c863180f84891c95656a4
Love Lysell Berglund <love.lysellberglund@learnet.se>
2024-02-01 10:11:34 +0100

    Added Save System

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,69 +1,71 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using System.IO;
 using UnityEngine;
+
+
+[Serializable]
+public class SaveData
+{
+    public float blood;
+    public int eyes;
+    public int killed;
+}
+
 
 public class GameManager : SingletonPersistent<MonoBehaviour>
 {
-    private float blood;
-    private int eyes;
-    private int killed;
-    
-    public float Blood
-    {
-        get
-        {
-            return blood;
-        }
-        set
-        {
-            blood = value;
-        }
-    }
-    
-    public int Eyes
-    {
-        get
-        {
-            return eyes;
-        }
-        set
-        {
-            int diff = value - eyes;
-            if (diff > 0)
-            {
-                killed += diff;
-            }
-            eyes = value;
-        }
-    }
+    [SerializeField] private TextMeshProUGUI eyesText;
 
+    private string savePath;
+    private SaveData saveData;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
+        savePath = Path.Combine(Application.persistentDataPath, "saveData.json");
         Load();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        saveData.eyes += 1;
+        eyesText.text = saveData.eyes.ToString();
 
-    private void Save()
-    {
-        PlayerPrefs.SetFloat("blood", blood);
-        PlayerPrefs.SetInt("eyes", eyes);
-        PlayerPrefs.SetInt("killed", killed);
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Quit();
+        }
     }
     
     private void Load()
     {
-        blood = PlayerPrefs.GetFloat("blood");
-        eyes = PlayerPrefs.GetInt("eyes");
-        killed = PlayerPrefs.GetInt("killed");
+        if (File.Exists(savePath))
+        {
+            string json = File.ReadAllText(savePath);
+            saveData = JsonUtility.FromJson<SaveData>(json);
+        }
+        else
+        {
+            saveData = new SaveData();
+        }
     }
 
+    private void Save()
+    {
+        string json = JsonUtility.ToJson(saveData);
+        File.WriteAllText(savePath, json);
+    }
+
+    private void Quit()
+    {
+        Application.Quit();
+    }
+    
     private void OnApplicationQuit()
     {
         Save();
